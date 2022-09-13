7b2295a4619569e7b78798ab12bf057dc3471d1d
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-09-13 09:11:20 +0100

    Initial Commit

---
+++ ReadingAppSwift/ReadingAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingAppSwiftApp.swift
+//  ReadingAppSwift
+//
+//  Created by Hin, Ethan-Scott (WING) on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadingAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
