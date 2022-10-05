4d206d98ace2948ea4eb8816339424736ad5241c
Ethan-Scott Hin <63197802+SpicyChair@users.noreply.github.com>
2022-10-05 13:02:37 +0100

    Update BookLogData.swift

--- ReadingAppSwift/Model/Book/BookLogData.swift
+++ ReadingAppSwift/Model/Book/BookLogData.swift
@@ -10,19 +10,50 @@ import Foundation
 
 class BookLogData: Codable {
     
+    // total amount of pages
     let pageCount: Int
-    var pageProgress: Int
+    
+    // total progress in pages
+    var pageProgress: Int = 0
+    
+    // dictionary of dates pointing to number of pages read
+    // on that particular day
     var pagesPerDay : [String : Int]
     
     func logPages(pages: Int) {
+        
+        // the amount of pages to add
+        var toAdd = 0
+        
+        // pages read can never be greater than the amount of pages in the book
         if (pageProgress + pages >= pageCount) {
-            let toAdd = pageCount - pageProgress
+            toAdd = pageCount - pageProgress
+        } else {
+            toAdd = pages
+        }
+        
+        // calculate dateAsString
+        // this is used as the key in the pagesPerDay dictionary
+        let date = Date()
+        let calendar = Calendar.current
+        let day = calendar.component(.day, from: date)
+        let month = calendar.component(.month, from: date)
+        let year = calendar.component(.year, from: date)
+        let dateAsString = "\(day)-\(month)-\(year)"
+
+        // if an entry exists for today, add the new pages
+        
+        if let currentPages = pagesPerDay[dateAsString] {
+            pagesPerDay.updateValue(currentPages + toAdd, forKey: dateAsString)
             
+        
+        // else, create a new entry for today
             
         } else {
-            pageProgress += pages
+            pagesPerDay.updateValue(currentPages + toAdd, forKey: dateAsString)
         }
+        
+        // update the total progress
+        pageProgress += toAdd
     }
-    
-    
 }
