81632ccc9c659ce0e4b520de62b436da6a25d6ac
Ethan <63197802+SpicyChair@users.noreply.github.com>
2023-02-01 16:06:06 +0000

    fix users not updating

--- ReadingAppSwift/Screens/OnNavigationBar/ProfileScreenView.swift
+++ ReadingAppSwift/Screens/OnNavigationBar/ProfileScreenView.swift
@@ -14,6 +14,7 @@ struct ProfileScreenView: View {
     @State private var password = ""
     @State private var name = ""
     
+    @State var didAppear = false
     
     @State var showingSheet = false
     @State private var showingAlert = false
@@ -66,6 +67,12 @@ struct ProfileScreenView: View {
                     - Enter reading challenges
                     """
                     ).font(.system(size: 20, weight: .bold, design: .serif))
+                }.onAppear {
+                    if !didAppear {
+                        adapter.getChallenges()
+                        didAppear = true
+                    }
+                    adapter.getUsers()
                 }
                 
             }
